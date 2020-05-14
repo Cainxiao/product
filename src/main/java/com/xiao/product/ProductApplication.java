2242ca97c4cd882f66744d8ff33123451ca020a9
xiaoyanwu <924511485@qq.com>
2020-05-14 10:51:33 +0800

    1

--- src/main/java/com/xiao/product/ProductApplication.java
+++ src/main/java/com/xiao/product/ProductApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ProductApplication {
 
     //1
-    //2
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
